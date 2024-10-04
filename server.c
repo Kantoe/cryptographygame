1c330585d4699edcbbaa4a7650e0da6a523ff4ff
idokantor <kantor.ido@gmail.com>
2024-10-05 01:07:53 +0300

    second

--- server.c
+++ server.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <fcntl.h>
 #include "cryptography_game_util.h"
 #define CORRECT_ARGC 2
 #define SERVER_IP "0.0.0.0"
 #define CLIENT_MAX "max 2 clients can connect"
-#define COMMAND_BUFFER_SIZE 9
 
 struct AcceptedSocket
 {
@@ -38,7 +38,9 @@ void sendReceivedMessageToTheOtherClients(const char *buffer,int socketFD);
 
 void cleanupThreads(int count);
 
-struct AcceptedSocket acceptedSockets[2] ;
+void cleanupClientSockets(int count);
+
+struct AcceptedSocket acceptedSockets[2];
 int acceptedSocketsCount = 0;
 pthread_t clientThreads[2] = {0};
 
@@ -49,7 +51,7 @@ void startAcceptingIncomingConnections(const int serverSocketFD)
         if(acceptedSocketsCount < 2)
         {
             struct AcceptedSocket clientSocket  = acceptIncomingConnection(serverSocketFD);
-            if (clientSocket.acceptedSuccessfully)
+            if(clientSocket.acceptedSuccessfully)
             {
                 acceptedSockets[acceptedSocketsCount++] = clientSocket;
                 receiveAndPrintIncomingDataOnSeparateThread(&clientSocket);
@@ -61,6 +63,7 @@ void startAcceptingIncomingConnections(const int serverSocketFD)
             send(clientSocketFD, CLIENT_MAX, strlen(CLIENT_MAX), 0);
             close(clientSocketFD);
         }
+        usleep(100000);
     }
 }
 
@@ -75,7 +78,7 @@ void * receiveAndPrintIncomingData(void * arg)
 {
     const int clientSocketFD = (intptr_t)arg;
     char buffer[1024];
-    while(1)
+    while(!stop)
     {
         const ssize_t amountReceived = recv(clientSocketFD, buffer,1024,0);
         if(amountReceived > 0)
@@ -105,9 +108,9 @@ struct AcceptedSocket acceptIncomingConnection(const int serverSocketFD)
 {
     struct sockaddr_in clientAddress ;
     int clientAddressSize = sizeof(struct sockaddr_in);
+    struct AcceptedSocket acceptedSocket = {0};
     const int clientSocketFD = accept(serverSocketFD,
         (struct sockaddr*)&clientAddress, (socklen_t *)&clientAddressSize);
-    struct AcceptedSocket acceptedSocket;
     acceptedSocket.address = clientAddress;
     acceptedSocket.acceptedSocketFD = clientSocketFD;
     acceptedSocket.acceptedSuccessfully = clientSocketFD > 0;
@@ -123,7 +126,21 @@ void cleanupThreads(const int count)
     for (int i = 0; i < count; i++)
     {
         if(clientThreads[i] != 0)
+        {
+            pthread_cancel(clientThreads[i]);
             pthread_join(clientThreads[i], NULL);  // Wait for each thread to finish
+        }
+    }
+}
+
+void cleanupClientSockets(const int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if(acceptedSockets[i].acceptedSocketFD > 0)
+        {
+            close(acceptedSockets[i].acceptedSocketFD);
+        }
     }
 }
 
@@ -135,10 +152,10 @@ int main(const int argc, char *argv[])
         printf("incorrect number of arguments\n");
         return EXIT_FAILURE;
     }
-
     const int serverSocketFD = createTCPIpv4Socket();
     struct sockaddr_in server_address;
     createIPv4Address(SERVER_IP, atoi(argv[1]), &server_address);
+    fcntl(serverSocketFD, F_SETFL, O_NONBLOCK);
     const int result = bind(serverSocketFD, (struct sockaddr *)&server_address, sizeof(server_address));
     if(result == 0)
         printf("socket was bound successfully\n");
@@ -153,6 +170,7 @@ int main(const int argc, char *argv[])
     }
     startAcceptingIncomingConnections(serverSocketFD);
     cleanupThreads(acceptedSocketsCount);
+    cleanupClientSockets(acceptedSocketsCount);
     shutdown(serverSocketFD,SHUT_RDWR);
     close(serverSocketFD);
     return 0;
